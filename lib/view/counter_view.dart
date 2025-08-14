efaf0a243905b64c6604d8e7d90a026bab6b4e96
boni783 <022200797e@uandina.edu.pe>
2025-08-14 17:05:42 -0500

    version1

---
+++ lib/view/counter_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class counterView extends StatelessWidget {
+  const counterView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar(), body: const Text("Hola mundo"));
+  }
+}
